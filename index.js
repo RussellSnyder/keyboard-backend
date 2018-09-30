536e94590ee0e7c344a157642dbc57e31e0e5c67
RussellSnyder <russell@rustybass.com>
2018-10-01 01:11:05 +0200

    now in version 2 working

--- index.js
+++ index.js
@@ -4,30 +4,42 @@ const songs = [
     {
         id: 1,
         title: 'Arpeggios for the Win',
-        tempo: '120',
+        tempo: 120,
         keysPlayed: [ 'C4', 'D4', 'E4', 'C4', 'E4', 'D4', 'C4'],
-        keysPlayedLength: [ '4n', '8n','8n','4n','4n','4n', '4n'],
+        keysPlayedLength: [ 0.25, 0.125,0.125,0.25,0.25,0.25, 0.25],
+        keysPlayedOnset: [0, 0.25, 0.375, 0.5, 0.75, 1, 1]
     },
     {
         id: 2,
         title: 'Minor Victory',
-        tempo: '160',
+        tempo: 80,
         keysPlayed: [ 'C4', 'D4', 'Eb4', 'C4', 'D4', 'Eb4', 'C5', 'D5', 'Eb5', 'C4'],
-        keysPlayedLength: [ '4n', '8n','8n','4n','4n','4t', '4t', '4t', '4n', '8n'],
+        keysPlayedLength: [ 0.25, 0.125,0.125,0.25,0.25,0.33, 0.33, 0.33, 0.25, 0.125],
+        keysPlayedOnset: [0, 0.25, 0.375, 0.5, 0.75, 1, 1.33, 1.66, 2, 3]
     },
     {
         id: 3,
         title: 'Fifths to Freedom',
-        tempo: '80',
+        tempo: 80,
         keysPlayed: [ 'C4', 'G4', 'C4', 'G4', 'Db4', 'Ab4', 'Db4', 'Ab4', 'C5', 'C4'],
-        keysPlayedLength: [ '4t', '4t', '4t', '4t', '4t', '4t', '4t', '4t', '4t', '4n'],
+        keysPlayedLength: [ 0.33, 0.33, 0.33, 0.33, 0.33, 0.33, 0.33, 0.33, 0.33, 0.25],
+        keysPlayedOnset: [0, 0.33, 0.66, 1, 1.33, 2.66, 3, 3.33, 3.66, 4]
     },
     {
         id: 4,
         title: 'Lightening Licks',
-        tempo: '280',
+        tempo: 280,
         keysPlayed: [ 'E4', 'E4', 'F4', 'G4', 'E4','C4', 'D4', 'D4', 'F4', 'E4', 'D4', 'E5','E5', 'F5', 'G5', 'E5','C5', 'D5', 'D5', 'F5', 'E5', 'D5'],
-        keysPlayedLength: [ '8n','8n','8n','8n', '4n', '8n', '4n', '8n', '8n', '8n', '4n', '8n','8n','8n','8n', '4n', '8n', '4n', '8n', '8n', '8n', '4n'],
+        keysPlayedLength: [ 0.125,0.125,0.125,0.125, 0.25, 0.125, 0.25, 0.125, 0.125, 0.125, 0.25, 0.125,0.125,0.125,0.125, 0.25, 0.125, 0.25, 0.125, 0.125, 0.125, 0.25],
+        keysPlayedOnset: [0, 0.125, 0.25, 0.375, 0.6, 1, 1.13, 1.3, 1.5, 1.75, 2, 2.125, 2.25, 2.375, 2.6, 3, 4.13, 4.3, 4.5, 4.75, 5, 6]
+    },
+    {
+        id: 5,
+        title: 'Overlap',
+        tempo: 120,
+        keysPlayed: [ 'Gb4', 'B4', 'D5', 'Gb4', 'B4', 'D5', 'Gb5', 'B5', 'Gb5', 'D5', 'Gb4', 'B4', 'D5', 'Gb4', 'B4', 'D5', 'Gb5', 'B5', 'Gb5', 'D5', 'B4', 'F#4'],
+        keysPlayedLength: [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 2, 1, 3, 3],
+        keysPlayedOnset: [ 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 9, 10, 11, 12, 12]
     }
 ];
 
@@ -35,9 +47,10 @@ const typeDefs = gql`
     type Song {
         id: ID!
         title: String
-        tempo: String
+        tempo: Float
         keysPlayed: [String]
-        keysPlayedLength: [String]
+        keysPlayedLength: [Float]
+        keysPlayedOnset: [Float]
     }
 
     type Query {
@@ -46,8 +59,8 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        addSong(title: String, tempo: String, keysPlayed: [String], keysPlayedLength: [String]): Song
-        updateSong(id: ID!, title: String, tempo: String, keysPlayed: [String], keysPlayedLength: [String]): Song
+        addSong(title: String, tempo: Float, keysPlayed: [String], keysPlayedLength: [Float], keysPlayedOnset: [Float]): Song
+        updateSong(id: ID!, title: String, tempo: Float, keysPlayed: [String], keysPlayedLength: [Float], keysPlayedOnset: [Float]): Song
     }
     
 `
@@ -57,19 +70,20 @@ const resolvers = {
         songs: () => songs,
     },
     Mutation: {
-        addSong: (_, { title, tempo, keysPlayed, keysPlayedLength }) => {
+        addSong: (_, { title, tempo, keysPlayed, keysPlayedLength, keysPlayedOnset }) => {
             const newSong = { 
                 id: songs.length + 1,
                 title,
                 tempo,
                 keysPlayed,
-                keysPlayedLength
+                keysPlayedLength,
+                keysPlayedOnset
             };
             songs.push(newSong);
 
             return newSong;
         },
-        updateSong: (_, { id, title, tempo, keysPlayed, keysPlayedLength }) => {
+        updateSong: (_, { id, title, tempo, keysPlayed, keysPlayedLength, keysPlayedOnset }) => {
             let songIndexToUpdate = songs.findIndex(x => x.id == id);
 
             let updatedState = {
@@ -77,7 +91,8 @@ const resolvers = {
                 title,
                 tempo,
                 keysPlayed,
-                keysPlayedLength
+                keysPlayedLength,
+                keysPlayedOnset
             }
 
             songs[songIndexToUpdate] = updatedState
